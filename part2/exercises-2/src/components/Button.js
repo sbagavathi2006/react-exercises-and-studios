e0fe96be72e1dd865507da84661e6c2c3f15cc0a
Bagavathi <sbagavathi2006@gmail.com>
2023-08-04 12:19:29 +0530

    completed part2 exercise

--- part2/exercises-2/src/components/Button.js
+++ part2/exercises-2/src/components/Button.js
@@ -1,12 +1,17 @@
 import './styles.css';
 
 function Button() {
+   
+   function onLearnMore(){
+      alert("Slipsh Splash");
+};
 
    return ( 
-         <button>
+         <button onClick={onLearnMore}>
             Learn More
          </button>
    );
 }
+
 
 export default Button;
--- part2/exercises-2/src/components/Profile.js
+++ part2/exercises-2/src/components/Profile.js
@@ -1,14 +1,27 @@
 import './styles.css';
 import Button from './Button';
+import oceans from './oceans.json';
 
 
 function Profile()
-{
+{   
+   const listItem = oceans.map(ocean=>
+   <div key={ocean.id} className={`${ocean.fishCheck==="true" ?"isAFish":"profile"}`}>
+      <h1>{ocean.name}</h1>
+      <img src={ocean.image} alt={ocean.name} className='img'/>
+      <h3> Fun Facts: </h3>
+         <ol>
+            <li>{ocean.fact1}</li>
+            <li>{ocean.fact2}</li>
+            <li>{ocean.fact3}</li>
+         </ol> 
+      <Button />
+   </div>
+   );
    return(
-      <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
-         <Button />
-      </>  
+      <ul>
+         {listItem}
+      </ul>  
    );
 }
 
