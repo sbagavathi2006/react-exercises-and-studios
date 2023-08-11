2d0728173a6eb4518ab4855aa690aab7bae9da9a
Bagavathi <sbagavathi2006@gmail.com>
2023-08-11 06:23:12 +0530

    done

--- part3/studio/src/components/BoardAssignment.js
+++ part3/studio/src/components/BoardAssignment.js
@@ -1,18 +1,31 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-export default function BoardAssignment () {
-   const boards = [];
+export default function BoardAssignment() {
+  const [boardName, setName] = useState("no boards yet!");
+  const boards = [
+    { label: 1, value: "Easy Diners" },
+    { label: 2, value: "Pasta Meals" },
+    { label: 3, value: "Fall Favorites" },
+  ];
 
-   const handleChange = (event) => {
-   }
+  const handleChange = (event) => {
+    setName(event.target.value);
+  };
 
-   return (
-      <div style={{paddingTop: "50px"}}>
+  return (
+    <div style={{ paddingTop: "50px" }}>
       <label>Save to Board: </label>
       <select value={boardName} onChange={handleChange}>
+        <option value="">Select a board</option>
+
+        {boards.map((board) => (
+          <option key={board.label} value={board.value}>
+            {board.value}
+          </option>
+        ))}
       </select>
 
       <p>Saved to {boardName}!</p>
-      </div>
-   );
+    </div>
+  );
 }
--- part3/studio/src/components/Recipe.js
+++ part3/studio/src/components/Recipe.js
@@ -1,63 +1,73 @@
 const RecipeAuthor = () => {
-   let authorLink = "";
-   let authorPhoto = "";
-   let authorName = "";
-
+   let authorLink =
+     "https://www.inspiredtaste.net/wp-content/uploads/2023/04/Adam-and-Joanne-2-1200.jpg";
+   let authorPhoto =
+     "https://www.inspiredtaste.net/wp-content/uploads/2023/04/Adam-and-Joanne-2-1200.jpg";
+   let authorName = "Adam";
+ 
    return (
-      <div>
-         <img src={authorPhoto} alt = "" style={{objectFit: "contain", borderRadius: "50%"}} />
-         <div>
-            <h3>{authorName}</h3>
-            <a href={authorLink}></a> 
-         </div>
-      </div>
+     <div>
+       <img
+         src={authorPhoto}
+         alt=""
+         style={{ objectFit: "contain", borderRadius: "50%" }}
+       />
+       <div>
+         <h3>{authorName}</h3>
+         <a href={authorLink}></a>
+       </div>
+     </div>
    );
-}
-
-const RecipeIngredients = () => {
-   const ingredients = [];
-   return(
-      <div>
-         <h3>Recipe Ingredients</h3>
-         <ul>
-            <li>{ingredients[0]}</li>
-            <li>{ingredients[1]}</li>
-            <li>{ingredients[2]}</li>
-            <li>{ingredients[3]}</li>
-            <li>{ingredients[4]}</li>
-         </ul>
-      </div>
-   );
-}
-
-const RecipeDescription = () => {
+ };
+ 
+ const RecipeIngredients = () => {
+   const ingredients = ["beef", "olive oil", "onion", "garlic", "tomato paste"];
    return (
-      <div> 
-         <div>
-            <h1></h1>
-            <p></p>
-         </div>
-         <div className="recipePhotoBlock">
-            <RecipeIngredients />
-            <RecipeAuthor />
-         </div>
-      </div>
+     <div>
+       <h3>Recipe Ingredients</h3>
+       <ul>
+         <li>{ingredients[0]}</li>
+         <li>{ingredients[1]}</li>
+         <li>{ingredients[2]}</li>
+         <li>{ingredients[3]}</li>
+         <li>{ingredients[4]}</li>
+       </ul>
+     </div>
    );
-}
-
-const RecipePhoto = () => {
+ };
+ 
+ const RecipeDescription = () => {
    return (
-      <img src="" alt="" className="imageUpdates"/>
+     <div>
+       <div>
+         <h1>Weeknight Spaghetti</h1>
+         <p>It's Spaghetti</p>
+       </div>
+       <div className="recipePhotoBlock">
+         <RecipeIngredients />
+         <RecipeAuthor />
+       </div>
+     </div>
    );
-}
-
-export default function RecipeDisplay () {
-   return(
-      <div className="recipePhotoBlock">
-         <RecipePhoto />
-         <div>
-            <RecipeDescription />
-         </div>
-      </div>
+ };
+ 
+ const RecipePhoto = () => {
+   return (
+     <img
+       src="https://www.inspiredtaste.net/wp-content/uploads/2019/03/Spaghetti-with-Meat-Sauce-Recipe-3-1200.jpg"
+       alt="Spaghetti"
+       className="imageUpdates"
+     />
    );
-}
+ };
+ 
+ export default function RecipeDisplay() {
+   return (
+     <div className="recipePhotoBlock">
+       <RecipePhoto />
+       <div>
+         <RecipeDescription />
+       </div>
+     </div>
+   );
+ }
